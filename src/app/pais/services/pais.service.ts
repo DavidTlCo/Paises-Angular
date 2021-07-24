3560e219db97140678da02a6026ec6565c491e82
David_TC <davidcoraza0316@gmail.com>
2021-07-23 23:25:31 -0500

    Add an interface to show the reponse from petition

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,9 @@ export class PaisService {
   private baseUrl: string = 'https://restcountries.eu/rest/v2';
 
   constructor( private http: HttpClient ) { }
-
-  buscar( termino: string ): Observable<any>{
+  // This method converts the response to an array of countries and returns it
+  buscar( termino: string ): Observable<Country[]>{
     const url = `${ this.baseUrl }/name/${ termino }`;
-    return this.http.get( url );
+    return this.http.get<Country[]>( url );
   }
 }
